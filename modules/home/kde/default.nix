c59dae20f67f0a38952899b71666ca528911a1d7
Corona <Corona09@163.com>
2023-02-21 15:35:10 +0800

    fix path

--- modules/home/kde/default.nix
+++ modules/home/kde/default.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
     home.file.".config/kdeglobals".source = ./kdeglobals/kdeglobals;
-    home.file.".local/share/konsole/Breeze.colorscheme".source = ./konsole/Breeze.colorscheme;
+    home.file.".local/share/konsole/WhiteOnBlack.colorscheme".source = ./konsole/WhiteOnBlack.colorscheme;
     home.file.".local/share/konsole/Profile1.profile".source = ./konsole/Profile1.profile;
 }
